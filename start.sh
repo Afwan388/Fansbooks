cae55a8cb362b0847add85efb40515b069f79fbd
Afwan388 <afwanizzulfadhoil@gmail.com>
2024-06-16 22:23:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ if [ ! -d $VIRTUALENV ]; then
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi 
 
 $VIRTUALENV/bin/pip install -r requirements.txt
