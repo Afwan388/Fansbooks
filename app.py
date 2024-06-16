6071bc809fcb3315bacc15276cd860c97c47620d
Afwan388 <afwanizzulfadhoil@gmail.com>
2024-06-16 22:12:40 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/fansbook", methods=["POST"])
+def post_comment():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
+    }
+    db.fansmessages.insert_one(doc)
+    return jsonify({'msg':'successfully!'})
+
+@app.route("/fansbook", methods=["GET"])
+def get_comment():
+    message_list = list(db.fansmessages.find({}, {'_id': False}))
+    return jsonify({'comments': message_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ start.sh
@@ -0,0 +1,18 @@
+set -eu 
+
+export PTYHONUNBUFFERED=true
+
+VIRTUALENV=.data/venv
+
+if [ ! -d $VIRTUALENV ]; then
+    python3 -m venv $VIRTUALENV
+fi
+
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV
+fi 
+
+$VIRTUALENV/bin/pip install -r requirements.txt
+
+$VIRTUALENV/bin/python3 app.py 
+footer
